7d4419e74559b3ecfc955ba88ad31e7645cebd5c
yandrews572-a11y <yandrews572@gmail.com>
2026-08-13 00:22:28 +0530

    Prepare URL shortener for deployment

--- app.py
+++ app.py
@@ -145,6 +145,8 @@ def stats(short_code):
     })
 
 
-# Start Flask Server
+import os
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
